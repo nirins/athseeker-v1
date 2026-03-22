ec41da9bfa5a09b35e5c8bf93e665e4a8f7e5b9a
nirins <nirin.suarod@gmail.com>
2026-03-22 21:23:09 +0700

    browse without login

--- tradeseeker-web-v2/tradeseeker-web-v2/src/app/features/auth/login/login.component.ts
+++ tradeseeker-web-v2/tradeseeker-web-v2/src/app/features/auth/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    public router: Router
   ) {}
 
   ngOnInit(): void {
